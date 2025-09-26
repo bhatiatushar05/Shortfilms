9c3c9acd0d8c875da770c8e236a1a4358728354e
AbhigyanSadhanidar <abhigyansadhanidar@gmail.com>
2025-09-26 14:34:50 +0530

    Hello

--- ott-web/src/components/media/MovieTest.jsx
+++ ott-web/src/components/media/MovieTest.jsx
@@ -26,22 +26,22 @@ const MovieTest = () => {
       <h2>🎬 Streaming Test</h2>
 
       <MoviePlayer
-        hlsUrl="https://ott-movie-storage.s3.ap-south-1.amazonaws.com/movies/BigBuckBunny/master.m3u8"
+        hlsUrl="https://ott-movies-bucket-1.s3.us-east-1.amazonaws.com/movies/BigBuckBunny/master.m3u8"
         title="Big Buck Bunny"
       />
 
       <MoviePlayer
-        hlsUrl="https://ott-movie-storage.s3.ap-south-1.amazonaws.com/movies/ElephantsDream/hls/ElephantsDream.m3u8"
+        hlsUrl="https://ott-movies-bucket-1.s3.us-east-1.amazonaws.com/movies/ElephantsDream/hls/master.m3u8"
         title="Elephants Dream"
       />
 
       <MoviePlayer
-        hlsUrl="https://ott-movie-storage.s3.ap-south-1.amazonaws.com/movies/Sintel/hls/Sintel.m3u8"cd //
+        hlsUrl="https://ott-movies-bucket-1.s3.us-east-1.amazonaws.com/movies/Sintel/hls/Sintel.m3u8"
         title="Sintel"
       />
 
       <MoviePlayer
-        hlsUrl="https://ott-movie-storage.s3.ap-south-1.amazonaws.com/movies/TearsOfSteel/hls/TearsOfSteel.m3u8"
+        hlsUrl="https://ott-movies-bucket-1.s3.us-east-1.amazonaws.com/movies/TearsOfSteel/hls/TearsOfSteel.m3u8"
         title="Tears of Steel"
       />
 
